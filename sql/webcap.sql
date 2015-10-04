c567f768199efc2b773ada370cad07ff67254a7a
631068264 <631068264@qq.com>
2015-10-04 12:35:16 +0800

    更新数据库

---
+++ sql/webcap.sql
@@ -0,0 +1,78 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50096
+Source Host           : localhost:3306
+Source Database       : webcap
+
+Target Server Type    : MYSQL
+Target Server Version : 50096
+File Encoding         : 65001
+
+Date: 2015-10-04 12:33:19
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` bigint(20) NOT NULL,
+  `nick` varchar(64) NOT NULL COMMENT '昵称',
+  `password` varchar(64) NOT NULL COMMENT '密码',
+  `name` varchar(32) default NULL COMMENT '姓名',
+  `device` varchar(128) NOT NULL COMMENT '设备识别码',
+  `role_id` bigint(20) NOT NULL default '1' COMMENT '角色',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '0: normal, 1: deleted',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `nick` (`nick`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='账号表';
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL COMMENT '角色识别码 0:管理员,1:普通用户',
+  `name` varchar(32) NOT NULL COMMENT '角色名称',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('0', '管理员');
+INSERT INTO `role` VALUES ('1', '普通用户');
+
+-- ----------------------------
+-- Table structure for task
+-- ----------------------------
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(64) NOT NULL default 'Default' COMMENT '用户自定义任务名',
+  `src` varchar(200) default NULL COMMENT '资源——url',
+  `thumbnail` varchar(200) default NULL COMMENT '缩略图——url',
+  `create_time` datetime default NULL COMMENT '任务创建时间 按日期算',
+  `duration` datetime default NULL COMMENT '持续时间',
+  `interval` datetime default NULL COMMENT '时间间隔',
+  `size` bigint(20) default NULL COMMENT '资源大小',
+  `account_id` bigint(20) NOT NULL COMMENT '用户id',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '0: normal, 1: deleted',
+  PRIMARY KEY  (`id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务表';
+
+-- ----------------------------
+-- Records of task
+-- ----------------------------
