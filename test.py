72ac25d9e083f2cfab472a4591e3355051b0049c
631068264 <631068264@qq.com>
2015-10-09 23:00:50 +0800

    重构一下task 还有TODO

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import subprocess
 import shlex
 
+
 # size = 'ffprobe -v error -show_entries format=size -of default=noprint_wrappers=1:nokey=1 dump.mp4 > file'
 # proc = subprocess.Popen(shlex.split(size), shell=True)
 # time.sleep(1)
@@ -16,12 +17,16 @@ import shlex
 # print(size)
 # os.remove('file')
 
+def kill(proc):
+    while True:
+        if proc.poll() is not None:
+            proc.terminate()
+            break
+
+
 video = "ffmpeg -y -i rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp -c copy -t 10 dump.mp4"
 proc = subprocess.Popen(shlex.split(video), shell=True)
-while True:
-    p = proc.poll()
-    if p is not None:
-        proc.terminate()
-        break
+kill(proc)
+
 
 # os.remove('dump.mp4')
--- webcap/views/task.py
+++ webcap/views/task.py
@@ -3,10 +3,13 @@
 # __author__ = 'wuyuxi'
 import datetime
 import shlex
+import socket
 import subprocess
+import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from base.poolmysql import transaction
 from base.smartsql import Table as T, Field as F, QuerySet as QS
 from base.framework import db_conn
 from base import constant as const
@@ -24,38 +27,43 @@ def do_daily_task(db_writer):
     tasks = QS(db_writer).table(T.task).where(
         (F.create_time >= date["start"]) & (F.create_time <= date["end"]) &
         (F.status == const.TASK_STATUS.NORMAL)
-    ).order_by("create_time").select(
-        for_update=True)
+    ).order_by("create_time").select(for_update=True)
+
     for task in tasks:
         device = QS(db_writer).table(T.account).where(F.id == task.account_id).select_one("device")
         if device != 0 or device is not None:
-            do_task(device, task, db_writer)
-            # TODO:清楚redis remove_all_job()
             scheduler = BackgroundScheduler()
-            # scheduler.add_jobstore('redis', jobs_key=task.id, run_times_key=task.account_id)
             scheduler.add_job(do_task, 'interval', second=task.interval, args=[device, task, db_writer])
             scheduler.start()
 
 
 def do_task(device, task, db):
-    # TODO: IP 和 src待定 时间判定 video 大小
-    ip = ""
+    # TODO: IP 和 src待定 时间判定
+    ip = socket.gethostbyname(socket.gethostname())
     src = ""
     thumbnail = ""
     video = 'ffmpeg -i rtsp://' + ip + device + '.sdp -c copy -t ' + task.duration + src
     thumb = 'ffmpeg -i rtsp://' + ip + device + '.sdp -f image2 -t 0.001 -s 352x240' + thumbnail
 
-    subprocess.Popen(shlex.split(thumb), shell=True)
-    subprocess.Popen(shlex.split(video), shell=True)
+    kill(subprocess.Popen(shlex.split(thumb), shell=True))
+    kill(subprocess.Popen(shlex.split(video), shell=True))
 
-    QS(db).table(T.task).where(F.id == task.id).update({
-        "src": src,
-        "thumbnail": thumbnail,
-        "size": "",
-    })
+    with transaction(db) as trans:
+        # TODO:每个用户都有限定size
+        QS(db).table(T.task).where(F.id == task.id).update({
+            "src": src,
+            "thumbnail": thumbnail,
+            "size": os.path.getsize(src),
+        })
+        trans.finish()
 
 
 def get_today_range(today=datetime.date.today()):
+    """
+    获取一天界限
+    :param today:
+    :return:
+    """
     today_start = today
     today_end = today + datetime.timedelta(1)
     data = {
@@ -63,3 +71,15 @@ def get_today_range(today=datetime.date.today()):
         "end": today_end,
     }
     return data
+
+
+def kill(proc):
+    """
+    关闭子进程
+    :param proc:
+    :return:
+    """
+    while True:
+        if proc.poll() is not None:
+            proc.terminate()
+            break
