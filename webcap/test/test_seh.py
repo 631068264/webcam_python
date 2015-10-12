7471f705e29b1c8a6116eb6948b3aed41fdd9ac2
wyx <wyx@youjianghr.com>
2015-10-12 16:34:38 +0800

    some tips

--- webcap/test/test_seh.py
+++ webcap/test/test_seh.py
@@ -18,6 +18,9 @@ from base.smartsql import Table as T, Field as F, QuerySet as QS
 db = MySQLdbConnection(**config.db_config["db_writer"])
 
 
+# TODO:联机调试shed
+# TODO:多线程并行
+# TODO:大改数据库结构
 def daily_task():
     date = get_today_range()
     tasks = QS(db).table((T.task__t * T.account__a).on(F.a__id == F.t__account_id)).where(
